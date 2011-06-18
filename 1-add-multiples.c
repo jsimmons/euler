6f0f09c363a1ca34674b7f8efea1638b7c7cb17e
Joshua Simmons <simmons.44@gmail.com>
2011-06-18 13:37:35 +1000

    A more elegant solution
    Calculates the sum directly.

--- 1-add-multiples.c
+++ 1-add-multiples.c
@@ -2,17 +2,16 @@
 
 static const int MAX = 1000;
 
+static int sum_multiples(int multiple, int to)
+{
+    int n = (to - 1) / multiple;
+    return n * (n + 1) / 2 * multiple;
+}
+
 int main()
 {
-    int sum = 0;
-
-    for(int i = 1; i < MAX; i++)
-    {
-        if(i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
+    // Inclusion/Exclusion
+    int sum = sum_multiples(3, MAX) + sum_multiples(5, MAX) - sum_multiples(15, MAX);
 
     printf("%i\n", sum);
 
