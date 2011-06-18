0f484e86b820d689ed8044afde927b482eb68b04
Joshua Simmons <simmons.44@gmail.com>
2011-06-18 13:28:37 +1000

    Simple brute force solution

--- 1-add-multiples.c
+++ 1-add-multiples.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+
+static const int MAX = 1000;
+
 int main()
 {
+    int sum = 0;
+
+    for(int i = 1; i < MAX; i++)
+    {
+        if(i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("%i\n", sum);
+
     return 0;
 }
